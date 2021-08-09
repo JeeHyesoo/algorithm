3cbcf30e365ed037bee4ee6b8101ee7093fab524
Hyesoo Jee <hyesoo.jee@gmail.com>
2021-08-09 22:24:53 +0900

    20210809
    20210809

---
+++ 13_팰린드롬연결리스트.py
@@ -0,0 +1,36 @@
+import collections
+
+def isPalindrome(head):
+    q = collections.deque()
+
+    if not head:
+        return True
+
+    node = head
+
+    while node is not None:
+        q.append(node.val)
+        node = node.next
+
+    while len(q) > 1:
+        if q.popleft() != q.pop():
+            return False
+
+    return True
+
+
+def isPalindrome2(head):
+    rev = None
+    slow = fast = head
+
+    while fast and fast.next:
+        fast = fast.next.next:
+        rev, rev.next, slow = slow, rev, slow.next
+
+    if fast:
+        slow = slow.next
+
+    while rev and rev.val == slow.val:
+        slow, rev = slow.next, rev.next
+    
+    return not rev
