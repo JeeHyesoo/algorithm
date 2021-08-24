0f75ceaaa07070878adfe087295a755c98344872
Hyesoo Jee <hyesoo.jee@gmail.com>
2021-08-24 22:19:01 +0900

    20210824
    20210824

---
+++ 29_보석과돌.py
@@ -0,0 +1,15 @@
+def numJewelsInStones(J, S):
+    freqs = {}
+    count = 0
+
+    for char in S:
+        if char not in freqs:
+            freqs[char] = 1
+        else:
+            freqs[char] += 1
+
+    for char in J:
+        if char in freqs:
+            count += freqs[char]
+
+    return count
